f9a5c3194d6558f5f8f497813dc02e0544c58912
Ben Laufer <nlaufer@calpoly.edu>
2023-05-08 22:47:11 -0700

    added summary functions

--- R/summaryFunctions.R
+++ R/summaryFunctions.R
@@ -5,3 +5,60 @@
 #'
 #' @export
 
+winningTeam <- function(year){
+  if (!is.numeric(year)) {
+    stop("Input variable must be numeric.")
+    warning("Input variable is not numeric.")
+  }
+
+  WCList <- tidyWorldCup()
+  worldCups <- WCLIst[[3]]
+  worldCups <- worldCups %>%
+    filter(Year == year)
+
+  return(worldCups[Country])
+
+}
+
+#' Returns the years won for a given country input
+#'
+#' @return dataframe of numeric values
+#'
+#' @export
+
+yearsWon <- function(country){
+
+  if (!is.character(country)) {
+    stop("Input variable must be character.")
+    warning("Input variable is not character.")
+  }
+
+  WCList <- tidyWorldCup()
+  worldCups <- WCList[[3]]
+  worldCups <- worldCups %>%
+    filter(country == {{country}})
+
+  return(worldCups[Year])
+
+}
+
+#' Returns the information for a given player name
+#'
+#' @return dataframe with numeric/char information about player
+#'
+#' @export
+
+getPlayerInfo <- function(name){
+
+  if (!is.character(name)) {
+    stop("Input variable must be character.")
+    warning("Input variable is not character.")
+  }
+  WCList <- tidyWorldCup()
+  worldCups <- WCList[[2]]
+  worldCups <- worldCups %>%
+    filter(worldCups[7] == {{name}})
+
+  return(worldCups)
+
+}
