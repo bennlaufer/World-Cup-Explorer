1bdc8b2c101ad4292882ca41880c84464dea76a9
nberzinsCP <81826367+nberzinsCP@users.noreply.github.com>
2023-05-08 21:05:15 -0700

    Fixed Directories Issues
    Fixed directory issues and finally produced the correct output from the function

--- R/Functions Temp.R
+++ R/Functions Temp.R
@@ -1,16 +1,16 @@
-#' Produces a tidy dataframe of world cup data for the user
+#' Produces a list of tidy data frames of world cup data for the user
 #'
 #' @return A data Frame
 #'
-#' @importFrom readr read.csv()
-#'
 #' @export
-
-
 tidyWorldCup <- function() { #produces a list of different WC data frames for the user to use for other functions in the package
 
-  data_dir <- system.file("data", package = "rpack")
-  worldCupData <- readRDS(file.path(data_dir, "Rdata.rds")) # "Today Nils Learned..." how R compresses data upon package installation
+  data_dir_matches <- system.file("extdata","WorldCupMatches.csv", package = "rpack")
+  data_dir_players <- system.file("extdata","WorldCupPlayers.csv", package = "rpack")
+  data_dir_cups <- system.file("extdata","WorldCups.csv", package = "rpack")
+  worldCupData <- list(utils::read.csv(data_dir_matches),
+                       utils::read.csv(data_dir_players),
+                       utils::read.csv(data_dir_cups)) # "Today Nils Learned..." how R compresses data upon package installation
 
   return(worldCupData)
 
